3aa6573fe33057ed72a80b856fb21569c54320da
v.alafev <v.alafev@dextechnology.com>
2025-07-29 13:59:56 +0300

    fix and ready for build

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
 import React from "react";
 
-interface NotFoundProps {}
-
-export default function NotFound({}: NotFoundProps) {
+export default function NotFound() {
   return (
     <div className="min-h-screen bg-gray-50 py-8">
       <div className="max-w-4xl mx-auto px-4">
