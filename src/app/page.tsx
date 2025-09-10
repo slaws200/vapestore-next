586c3f718a392fc9c49ed0999be7af8bde0282aa
v.alafev <v.alafev@dextechnology.com>
2025-09-10 16:36:20 +0300

    change ssg component to ssr and add wakeup server method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-// import SearchInput from "@/components/SearchInput";
 import ProductListWithCategories from "@/components/productComponents/ProductListWithCategories";
 import { fetchAllCategories } from "@/lib/categories";
 import { fetchAllProducts } from "@/lib/products";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const initialProducts = await fetchAllProducts(0, 11);
   const initialCategories = await fetchAllCategories();
+  try {
+    fetch("https://mybot-pmod.onrender.com/", { method: "GET" });
+  } catch (error) {
+    console.error("Ошибка при разбудке сервера:", error);
+  }
   return (
     <main className="overflow-y-hidden scrollbar-hide">
       <div className="mx-auto px-4">
